9bee67b5aade6db8d636614a71b036617ecb2b24
Kiarash-Shahedi <85554088+kiarashshahedi@users.noreply.github.com>
2024-11-17 16:33:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ let currentLang = 'en';
 
 toggleButton.addEventListener('click', () => {
     currentLang = currentLang === 'en' ? 'fa' : 'en';
-    toggleButton.textContent = currentLang === 'en' ? 'Switch to فارسی' : 'Switch to English';
+    toggleButton.textContent = currentLang === 'en' ? 'Switch to فارسی' : 'تغییر به English';
     
     elementsToTranslate.forEach(element => {
         element.textContent = element.getAttribute(`data-${currentLang}`);
     });
+
+    // Update text direction for Persian
+    document.body.style.direction = currentLang === 'fa' ? 'rtl' : 'ltr';
 });
